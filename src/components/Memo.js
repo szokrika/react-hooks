a679d95e85ca0cda8758b3314695093da490093c
Csongor Széles <szokrika@gmail.com>
2019-04-02 08:34:45 -0600

    updates

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -4,9 +4,28 @@ const computeExpensiveValue = (x, y) => {
   return x * y;
 };
 
+const divisible = (number = 1, division = 1) => number % division === 0;
+
 export default function Memo({ a, b }) {
   // it should only run the function when a has changed
   const expensiveValue = useMemo(() => computeExpensiveValue(a, b), [a]);
+  const divider = 260;
+  // memoization "pipelining"
+  // how a value depends on a different memoised value
+  const shouldCompute = useMemo(
+    () => {
+      return divisible(expensiveValue, divider);
+    },
+    [expensiveValue]
+  );
+
+  const divisibleValue = useMemo(
+    () => {
+      return shouldCompute ? expensiveValue / divider : 0;
+    },
+    [expensiveValue]
+  );
+  // end memoization "pipelining"
 
   return (
     <div className="component">
@@ -17,7 +36,8 @@ export default function Memo({ a, b }) {
           {
             a,
             b,
-            expensiveValue
+            expensiveValue,
+            divisibleValue
           },
           null,
           2
